bde3130c99b38bf82ef2adac11fcaea23808dbdc
Nikita <grad20062007@gmail.com>
2025-09-01 21:55:51 +0700

    Initial commit

---
+++ CREATE_TABLE.sql
@@ -0,0 +1,9 @@
+--создание таблицы book, в которой будут храниться данные о книгах
+
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50),
+    author VARCHAR(30),
+    price DECIMAL(8, 2),
+    amount INT
+);
