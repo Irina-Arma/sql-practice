90ad4ed0a448327cb3580d5e2da38bc3e849a1c0
Irina-Arma <irina.golovko1991@gmail.com>
2025-09-11 21:43:27 +0700

    update

--- 04_sql_data_modification.sql
+++ 04_sql_data_modification.sql
@@ -3,3 +3,19 @@
 */
 
 
+CREATE TABLE supply (
+    supply_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(50),
+    author VARCHAR(30),
+    price DECIMAL(8, 2),
+    amount INT
+);
+
+
+INSERT INTO supply (title, author, price, amount)
+VALUES
+    ('Лирика', 'Пастернак Б.Л.', 518.99, 2),
+    ('Черный человек', 'Есенин С.А.', 570.20, 6),
+    ('Белая гвардия', 'Булгаков М.А.', 540.50, 7),
+    ('Идиот', 'Достоевский Ф.М.', 360.80, 3);
+SELECT * FROM supply;
