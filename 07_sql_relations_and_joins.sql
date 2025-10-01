462205ed3dcca2a96a22e9b1f9f52e6f19a9f025
Irina-Arma <irina.golovko1991@gmail.com>
2025-10-01 19:06:30 +0700

    update

--- 07_sql_relations_and_joins.sql
+++ 07_sql_relations_and_joins.sql
@@ -35,4 +35,16 @@ CREATE TABLE book (
 );
 
 
-
+--Создаём таблицу book:
+--если удалить автора, все его книги удалятся автоматически (ON DELETE CASCADE);
+--если удалить жанр, в книгах этого жанра значение genre_id станет NULL (ON DELETE SET NULL)
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50),
+    author_id INT NOT NULL,
+    genre_id INT,
+    price DECIMAL(8,2),
+    amount INT,
+    FOREIGN KEY (author_id)  REFERENCES author (author_id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id)  REFERENCES genre (genre_id) ON DELETE SET NULL
+);
